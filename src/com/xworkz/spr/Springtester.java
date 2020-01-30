dba89274f24043d987bfbf67f699a65cec482863
kavyaggg <60472003+kavyaggg@users.noreply.github.com>
2020-01-30 18:51:18 +0530

    Add files via upload

---
+++ src/com/xworkz/spr/Springtester.java
@@ -0,0 +1,28 @@
+package com.xworkz.spr;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sri.spring.Threadexe.Labour;
+import com.sri.spring.Threadexe.ThreadIMPL;
+
+public class Springtester{
+	public static void main(String[] args) {
+		//ThreadIMPL th = new ThreadIMPL();
+		//th.start();
+		ApplicationContext con = new ClassPathXmlApplicationContext("context.xml");
+		Rocket reffofrocket=con.getBean(Rocket.class);
+		
+		Labour reffoflabour=con.getBean(Labour.class);
+		reffofrocket.launch();
+		reffoflabour.work();
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
